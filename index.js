0d771dbe562f8874d8f463c66d49f60b8cc23900
aryan12022 <choukseyaryan49@gmail.com>
2024-09-24 13:11:41 +0530

    add new change

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import 'dotenv/config'
 import express from 'express';
+
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
+
+
 const app = express(); 
 const port = process.env.PORT || 3000;
+app.use(
+   morgan(morganFormat, {
+     stream: {
+       write: (message) => {
+         const logObject = {
+           method: message.split(" ")[0],
+           url: message.split(" ")[1],
+           status: message.split(" ")[2],
+           responseTime: message.split(" ")[3],
+         };
+         logger.info(JSON.stringify(logObject));
+       },
+     },
+   })
+ );
 app.use(express.json())
  let teadata=[]
  let idea=1
  app.post('/teas',(req,res)=>{
+   logger.info("a post req is made to make a new tea")
     const {name , price}=req.body
     const newid={id:idea++,name,price};
     teadata.push(newid);
